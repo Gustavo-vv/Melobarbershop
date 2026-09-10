cfc3a3f93c302cde52d93307baa443276940e7b7
Guilherme <guifrancisco2002@outlook.com>
2026-09-10 20:19:47 -0300

    Cria o AuthController e o método GerarToken()

---
+++ Melobarbershop.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Melobarbershop.Domain.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Melobarbershop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        private JwtSecurityToken GerarToken(List<Claim> authClaims)
+        {
+            var authSigninKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Chave"]!));
+            var token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Emissor"],
+                    audience: _configuration["Jwt:Audiencia"],
+                    expires: DateTime.Now.AddHours(8),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigninKey, SecurityAlgorithms.HmacSha256)
+                );
+            return token;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<IActionResult> Registrar([FromBody] RegistrarUsuarioDto)
+        {
+
+        }
+    }
+}
